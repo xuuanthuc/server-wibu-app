edd1342641b4b3c850d73979990eb166999dcfac
saitohakira <62328990+saitohakira@users.noreply.github.com>
2021-05-24 10:12:29 +0700

    first push

---
+++ src/main/java/com/example/apiwibuapp/ApiWibuAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiwibuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWibuAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWibuAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apiwibuapp/ApiWibuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiwibuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWibuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
